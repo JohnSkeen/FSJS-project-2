c8ce5e33ad321bdc763829f4a89208b933a56723
John Skeen <jskeen@gmail.com>
2019-03-19 12:07:10 -0400

    minor changes

--- js/script.js
+++ js/script.js
@@ -68,6 +68,8 @@ addActive();
 //
 // changePage();
 
+
+// Attempting to find the parent index in order to change the page variable to current parent index
 const g = document.querySelectorAll('[href="#"]');
 console.log(g.length);
 for (let i = 0, len = g.length; i < len; i++)
@@ -76,33 +78,3 @@ for (let i = 0, len = g.length; i < len; i++)
         alert(g.parentNode.indexOf())  ;
     }
 }
-
-
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
-
-
-
-
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.

a4f841cc201158ed664849598d11543ccfb4e058
John Skeen <jskeen@gmail.com>
2019-03-19 12:06:04 -0400

    updating project

--- js/script.js
+++ js/script.js
@@ -1,63 +1,82 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+// Global Variables
 
-
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
-
-// const showPage = (list, page) => {
-//   for (i=0; i < list.length; i+=1) {
-//     if (list.index >= 0 && list.index <= 0) {
-//
-//     }
-//   }
-// };
-
-const liNodes = document.getElementsByClassName('student-item').length;
+const liNodes = document.getElementsByClassName('student-item');
 const newDiv = document.createElement('div');
-newDiv.className = "pagination";
 const pageLocation = document.querySelector('.page').appendChild(newDiv);
+let currentPage = 1;
+newDiv.className = "pagination";
 
+
+// Function to show students based on current page pageNumber
+
+const showPage = (list, page) => {
+  let start = ((page - 1) * 10);
+  let end = (start + 9);
+  for (i = 0; i < list.length; i +=1 ) {
+    if (i >= start && i <= end) {
+      list[i].style.display = 'block';
+    } else {
+      list[i].style.display = 'none';
+    }
+  }
+};
+
+// Function to add dynamic page list and set current active pageNumber
 
 const appendPageLinks = (list) => {
   let pageNumber = Math.ceil(list / 10);
-  console.log(pageNumber);
-  let createDiv = `
-    <ul>`
-    for (i = 0; i <= pageNumber; i+=1) {
-      createDiv += `
+  let createList = `
+    <ul id="pageList">`
+    for (i = 1; i <= pageNumber; i+=1) {
+      createList += `
       <li>
         <a href="#">${[i]}</a>
       </li>
       `
     }
-  createDiv += `
+  createList += `
     </ul>`
-
-  console.log(createDiv);
-
-  pageLocation.innerHTML = createDiv;
+  pageLocation.innerHTML = createList;
+  showPage(liNodes, currentPage);
 };
 
-appendPageLinks(liNodes);
+appendPageLinks(liNodes.length);
 
-const addActive = (list) => {
-  for (i=0; i<list.length; i+=0) {
-
+// add Active class to current page
+const addActive = () => {
+  const pageList = document.querySelectorAll('.pagination ul li');
+  for (i=0; i<pageList.length; i+=1) {
+    if (pageList[(currentPage - 1)] === pageList[i]) {
+      pageList[i].classList.add('active');
+    } else {
+      pageList[i].classList.remove('active');
+    }
   }
 };
+
+addActive();
+
+// const changePage = () => {
+//   const pageList = document.querySelectorAll('.pagination ul li a')
+//   console.log(pageList)
+//   // pageList.addEventListener('click', () => {
+//   //   currentPage = pageList
+//   // })
+//   // addActive();
+// };
+//
+// changePage();
+
+const g = document.querySelectorAll('[href="#"]');
+console.log(g.length);
+for (let i = 0, len = g.length; i < len; i++)
+{
+    g[i].onclick = function(){
+        alert(g.parentNode.indexOf())  ;
+    }
+}
+
 
 /***
    Create the `showPage` function to hide all of the items in the
